35db9972c5f9a5f3b61568cbc71cc755b208274e
Max Norton <maxbnorton@gmail.com>
2016-07-16 13:24:40 -0700

    style CDNR y-axis

--- js/figure.js
+++ js/figure.js
@@ -259,7 +259,7 @@ function the_figure(healthyYields, scenarioYieldObject, scenarioCDNRObject) {
 			.attr("y", 6)
 			.attr("dy", ".71em")
 			.style("text-anchor", "end")
-			.text("Yield (Tons/Acre)");
+			.text("Cumulative Discount Net Returns (2013 dollars)");
 
 		console.log(healthyData);
 		svg.append("path")
@@ -282,24 +282,6 @@ function the_figure(healthyYields, scenarioYieldObject, scenarioCDNRObject) {
 		for (var i in healthyYields) {
 			untreatedData[i] = { "x" : i, "y" : scenarioCDNRObject.untreated[i] };
 		}
-
-		x.domain(d3.extent(years));
-		y.domain(d3.extent(untreatedData, function(d) { return d.y; }));
-
-		svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
-
-		svg.append("g")
-			.attr("class", "y axis")
-			.call(yAxis)
-			.append("text")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", ".71em")
-			.style("text-anchor", "end")
-			.text("Yield (Tons/Acre)");
 
 		console.log(untreatedData);
 		svg.append("path")
