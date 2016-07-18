65f7446d5b72acdaeea790a892e249a36c8854c7
Max Norton <maxbnorton@gmail.com>
2016-07-18 16:37:44 -0700

    lint table.js

--- js/table.js
+++ js/table.js
@@ -12,6 +12,7 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 			acdnb75y10 = [],
 			ccthv = [],
 			costs = [],
+			compoundDiscount,
 			discountFactor = 1/(1+discount/100),
 			healthyACDNBna = [],
 			scenarioCDNRObject = {},
@@ -115,8 +116,7 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 			scenarioYieldObject.healthy[i] = {"x": i, "y": healthyYields[i]};
 			scenarioYieldObject.untreated[i] = {"x": i, "y": untreatedYields[i]};
 		}
-
-
+		
 		costs = [
 			cost0,
 			cost1,
@@ -149,28 +149,28 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 		var untreatedNR = [];
 		for (i in untreatedYields) {
 			untreatedNR[i] = price*untreatedYields[i]-costs[i];
-		};	
+		}
 
 		var untreatedDNR = [];
 		for (i in untreatedNR) {
-			var compoundDiscount = Math.pow(discountFactor, i);
+			compoundDiscount = Math.pow(discountFactor, i);
 			untreatedDNR[i] = untreatedNR[i]*compoundDiscount;
-		};	
+		}
 
 		var untreatedCDNR = [ untreatedDNR[0] ];
 		for (i=1; i<untreatedDNR.length; i++) {
 			untreatedCDNR[i] = untreatedDNR[i] + untreatedCDNR[i-1];
-		};
+		
 
  		var healthyCDNRna = [ price*healthyYields[0] - costs[0] ];
  		var healthyLPY = '-';
  		for (i=1; i<healthyYields.length; i++) {
- 			var compoundDiscount = Math.pow(discountFactor, i);
+ 			compoundDiscount = Math.pow(discountFactor, i);
  			healthyCDNRna[i] = (price*healthyYields[i] - costs[i])*compoundDiscount + healthyCDNRna[parseInt(i-1)];
  			if (healthyCDNRna[i] > healthyCDNRna[parseInt(i-1)]) {
  				healthyLPY = i;
  			}
- 		};
+ 		}
  		var healthyACDNBnaDisplay = healthyCDNRna[25] - untreatedCDNR[25];
  		if (healthyACDNBnaDisplay < 0) {
  			healthyACDNBnaDisplay = '-$' + parseFloat(-1*healthyACDNBnaDisplay).toFixed(2);
@@ -183,9 +183,9 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
  			if (healthyCDNRna[i+1] > 0) {
  				healthyBEAnaDisplay = i;
  				break;
- 			};
- 		};
- 		if (healthyBEAnaDisplay == -1) {
+ 			}
+ 		}
+ 		if (healthyBEAnaDisplay === -1) {
  			healthyBEAnaDisplay  = 'Never breaks even';
  			healthyLPY = '-';
  		}
@@ -193,13 +193,13 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
  		healthyACDNBna = [];
  		for (i in healthyCDNRna) {
  			healthyACDNBna[i] = healthyCDNRna[i] - untreatedCDNR[i];
- 		};
+ 		}
 
  		var untreatedLPY = 0;
 		while (untreatedDNR[untreatedLPY] < 0 && untreatedLPY < 25) {
  			untreatedLPY++;
  		}
- 		if (untreatedLPY==25) {
+ 		if (untreatedLPY === 25) {
  			untreatedLPY = 'Untreated vineyard never profitable';
  		} else {
 	 		while (untreatedDNR[untreatedLPY] > 0 && untreatedLPY < 25) {
@@ -209,8 +209,8 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 	 	}
 
  		var the_table_html = '<hr /><h2>Results</h2><section class="figure-area"></section><h3>Output table</h3><table><thead><th><h4>Scenario</h4></th><th><h4>ACDNB</h4></th><th><h4>Breakeven age</h4></th><th><h4>Last profitable year</h4></th><th><h4>Breakeven probability</h4></th></thead><tbody>';
-		the_table_html += '<tr><td>' + scenarios['healthy'] + '</td><td>' + healthyACDNBnaDisplay + '</td><td>' + healthyBEAnaDisplay + '</td><td>' + healthyLPY + '</td><td>' + 0 + '</td></tr>';
-		the_table_html += '<tr><td>' + scenarios['untreated'] + '</td><td>' + '-' + '</td><td>' + '-' + '</td><td>' + untreatedLPY + '</td><td>' + 1 + '</td></tr>';
+		the_table_html += '<tr><td>' + scenarios.healthy + '</td><td>' + healthyACDNBnaDisplay + '</td><td>' + healthyBEAnaDisplay + '</td><td>' + healthyLPY + '</td><td>' + 0 + '</td></tr>';
+		the_table_html += '<tr><td>' + scenarios.untreated + '</td><td>' + '-' + '</td><td>' + '-' + '</td><td>' + untreatedLPY + '</td><td>' + 1 + '</td></tr>';
 
  		for (var a=2; a<scenarioKeys.length; a++) {
 
@@ -221,26 +221,26 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
  			var pcFtnOfT = [];
 			for (var l=0; l<age; l++) {
 				pcFtnOfT[l] = 0;
-			};
-			for (var l=age; l<26; l++) {
+			}
+			for (l=age; l<26; l++) {
 				pcFtnOfT[l] = pc;
-			};
+			}
 
 			var healthyNR = [];
 			for (i in healthyYields) {
 				healthyNR[i] = price*healthyYields[i]-costs[i]-pcFtnOfT[i];
-			};
+			}
 
 			var healthyDNR = [];
 			for (i in healthyNR) {
-				var compoundDiscount = Math.pow(discountFactor, i);
+				compoundDiscount = Math.pow(discountFactor, i);
 				healthyDNR[i] = healthyNR[i]*compoundDiscount;
-			};
+			}
 
 			var healthyCDNR = [ healthyDNR[0] ];
 			for (i=1; i<healthyDNR.length; i++) {
 				healthyCDNR[i] = healthyDNR[i] + healthyCDNR[i-1];
-			};
+			}
 
 			for (i in healthyCDNR) {
 				scenarioCDNRObject.healthy[i] = {"x": i, "y": healthyCDNR[i]};
@@ -250,7 +250,7 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 			treatedYields = [];
 			for (i in healthyYields) {
 				treatedYields[i] = healthyYields[i]*data[i][selectCol]/100;
-			};
+			}
 
 			var thisKey = scenarioKeys[a];
 			scenarioYieldObject[thisKey] = [];
@@ -261,18 +261,18 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 			treatedNR = [];
 			for (i in treatedYields) {
 				treatedNR[i] = price*treatedYields[i]-costs[i]-pcFtnOfT[i];
-			};
+			}
 
 			treatedDNR = [];
 			for (i in treatedNR) {
-				var compoundDiscount = Math.pow(discountFactor, i);
+				compoundDiscount = Math.pow(discountFactor, i);
 				treatedDNR[i] = treatedNR[i]*compoundDiscount;
-			};
+			}
 
 			treatedCDNR = [ treatedDNR[0] ];
 			for (i=1; i<treatedDNR.length; i++) {
 				treatedCDNR[i] = treatedDNR[i] + treatedCDNR[i-1];
-			};
+			}
 
 			scenarioCDNRObject[thisKey] = [];
 			for (i in healthyYields) {
@@ -282,12 +282,12 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 			ccthv = [ parseInt(pcFtnOfT[0]) ];
 			for (i=1; i<healthyDNR.length; i++) {
 				ccthv[i] = parseInt(pcFtnOfT[i]) + parseInt(ccthv[i-1]);
-			};
+			}
 
 			var acdnb = [];
 			for (i in treatedCDNR) {
 				acdnb[i] = treatedCDNR[i] - untreatedCDNR[i];
-	 		};
+	 		}
 
 	 		switch (selectCol) {
 	 			case '25y3':
@@ -323,17 +323,17 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 	 			if (treatedCDNR[i] > untreatedCDNR[i]) {
 	 				bea[selectCol] = i;
 	 				break;
-	 			};
-	 		};
-	 		if (bea[selectCol]==0 && treatedCDNR[0] <= untreatedCDNR[0]) {
+	 			}
+	 		}
+	 		if (parseInt(bea[selectCol])===0 && treatedCDNR[0] <= untreatedCDNR[0]) {
 	 			bea[selectCol] = 'Never breaks even';
-	 		};
+	 		}
 
 	 		lpy[selectCol] = selectCol.substr(3);
 	 		while (treatedNR[parseInt(lpy[selectCol])+1] <= 0 && parseInt(lpy[selectCol])<25) {
 	 			lpy[selectCol]++;
 	 		}
-	 		if (parseInt(lpy[selectCol])==25) {
+	 		if (parseInt(lpy[selectCol])===25) {
 	 			lpy[selectCol] = 'Treatment never profitable';
 	 		} else {
 		 		while (treatedNR[parseInt(lpy[selectCol])+1] > 0 && parseInt(lpy[selectCol])<25) {
@@ -342,20 +342,21 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 		 	}
 
 	 		bep[selectCol] = (healthyCDNRna[25] - healthyCDNR[25]) / ( (treatedCDNR[25] - healthyCDNR[25]) - (untreatedCDNR[25] - healthyCDNRna[25]) );
-	 		if (bep[selectCol] > 1)
+	 		if (bep[selectCol] > 1) {
 	 			bep[selectCol] = 1;
+	 		}
 	 		bep[selectCol] = bep[selectCol].toFixed(3);
 
-	 		var acdnbDisplay = (acdnb[25] != null ) ? acdnb[25].toFixed(2) : '-';
-	 		if (acdnbDisplay != '-' && acdnbDisplay < 0) {
+	 		var acdnbDisplay = (acdnb[25] !== null ) ? acdnb[25].toFixed(2) : '-';
+	 		if (acdnbDisplay !== '-' && acdnbDisplay < 0) {
  				acdnbDisplay = '-$' + parseFloat(-1*acdnbDisplay).toFixed(2);
- 			} else if (acdnbDisplay != '-') {
+ 			} else if (acdnbDisplay !== '-') {
  				acdnbDisplay = '$' + parseFloat(acdnbDisplay).toFixed(2);
  			}
 
 			the_table_html += '<tr><td>' + scenarios[selectCol] + '</td><td>' + acdnbDisplay + '</td><td>' + bea[selectCol] + '</td><td>' + lpy[selectCol] + '</td><td>' + bep[selectCol] + '</td></tr>';
 
-	 	};
+	 	}
 
 		the_table_html += '</tbody></table>' + '<p class="print-link"><a href="javascript:window.print()"><i class="fa fa-print" aria-hidden="true"></i> Print these results.</a></p><p class="adjust-link"><a href="#form" onclick="$(\'body,html\').stop(true,true).animate({scrollTop: $(\'#form\').offset().top - $(\'header\').height()}, \'500\', \'swing\');">Adjust parameters</a></p>';
 		$('.results').html(the_table_html);
@@ -364,5 +365,8 @@ function the_table(discount, cost0, cost1, cost2, cost3, pc, price, yield0, yiel
 
 		$('body,html').stop(true,true).animate({scrollTop: $('#results').offset().top - $('header').height()}, '500', 'swing');
 
+		} //?
+
 	});
-};
+
+}
