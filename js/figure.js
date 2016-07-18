79316b647dff43c7ffc1eb1e4d79be38a505c922
Max Norton <maxbnorton@gmail.com>
2016-07-18 15:00:25 -0700

    restack figure lines, healthy then untreated on top

--- js/figure.js
+++ js/figure.js
@@ -1,12 +1,15 @@
 
 function the_figure(scenarioYieldObject, scenarioCDNRObject) {
 
-	var scenarioObject = false;
+	var scenarioObject = false,
+		dependentVariable;
 
 	if ( $('input[name=figuredisplay]:checked').val() == 'yield' ) {
 		scenarioObject = scenarioYieldObject;
+		dependentVariable = 'Yield (Tons / Acre)';
 	} else if ( $('input[name=figuredisplay]:checked').val() == 'netreturns' ) {
 		scenarioObject = scenarioCDNRObject;
+		dependentVariable = 'Cumulative Discounted Net Returns (2013 dollars)';
 	}
 
 	if (scenarioObject) {
@@ -17,16 +20,15 @@ function the_figure(scenarioYieldObject, scenarioCDNRObject) {
 
 		switch (parameterValue) {
 			case 'Year3':
-				scenarioName = ['healthy', '25y3', '50y3', '75y3', 'untreated'];
+				scenarioName = ['healthy', 'untreated', '25y3', '50y3', '75y3'];
 				break;
 			case 'Year5':
-				scenarioName = ['healthy', '25y5', '50y5', '75y5', 'untreated'];
+				scenarioName = ['healthy', 'untreated', '25y5', '50y5', '75y5'];
 				break;
 			case 'Year10':
-				scenarioName = ['healthy', '25y10', '50y10', '75y10', 'untreated'];
+				scenarioName = ['healthy', 'untreated', '25y10', '50y10', '75y10'];
 				break;
 		}
-		console.log(scenarioName);
 
 		var years = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
@@ -81,25 +83,31 @@ function the_figure(scenarioYieldObject, scenarioCDNRObject) {
 			.attr("y", 6)
 			.attr("dy", ".71em")
 			.style("text-anchor", "end")
-			.text("Cumulative Discounted Net Returns (2013 dollars)");
-
-		svg.append("path")
-			.attr("d", line(healthyData))
-			.attr("class", "line")
-			.attr("stroke", "blue")
-			.attr("stroke-width", 2)
-			.attr("fill", "none");
-
-		svg.selectAll("dot")
-			.data(healthyData)
-			.enter().append("circle")
-			.attr("r", 3.5)
-			.attr("fill","blue")
-			.attr("cx", function(d) { return x(d.x); })
-			.attr("cy", function(d) { return y(d.y); });
+			.text(dependentVariable);
 
 		for (var i in healthyYields) {
 			untreatedData[i] = { "x" : i, "y" : scenarioObject.untreated[i] };
+		}
+
+		if (parameterValue) {
+			var scenarioColors = ["yellowgreen", "darkorchid", "lightskyblue"];
+
+			for (var i in scenarioColors) {
+				svg.append("path")
+					.attr("d", line(scenarioObject[scenarioName[parseInt(i)+2]]))
+					.attr("class", "line")
+					.attr("stroke", scenarioColors[i])
+					.attr("stroke-width", 2)
+					.attr("fill", "none");
+
+				svg.selectAll("dot")
+					.data(scenarioObject[scenarioName[parseInt(i)+2]])
+					.enter().append("circle")
+					.attr("r", 3.5)
+					.attr("fill",scenarioColors[i])
+					.attr("cx", function(d) { return x(d.x); })
+					.attr("cy", function(d) { return y(d.y); });
+			}
 		}
 
 		svg.append("path")
@@ -117,28 +125,21 @@ function the_figure(scenarioYieldObject, scenarioCDNRObject) {
 			.attr("cx", function(d) { return x(d.x); })
 			.attr("cy", function(d) { return y(d.y); });
 
-		if (parameterValue) {
-			var scenarioColors = ["yellowgreen", "darkorchid", "lightskyblue"];
+		svg.append("path")
+			.attr("d", line(healthyData))
+			.attr("class", "line")
+			.attr("stroke", "blue")
+			.attr("stroke-width", 2)
+			.attr("fill", "none");
 
-			for (var i in scenarioColors) {
+		svg.selectAll("dot")
+			.data(healthyData)
+			.enter().append("circle")
+			.attr("r", 3.5)
+			.attr("fill","blue")
+			.attr("cx", function(d) { return x(d.x); })
+			.attr("cy", function(d) { return y(d.y); });
 
-				svg.append("path")
-					.attr("d", line(scenarioObject[scenarioName[parseInt(i)+1]]))
-					.attr("class", "line")
-					.attr("stroke", scenarioColors[i])
-					.attr("stroke-width", 2)
-					.attr("fill", "none");
-
-				svg.selectAll("dot")
-					.data(scenarioObject[scenarioName[parseInt(i)+1]])
-					.enter().append("circle")
-					.attr("r", 3.5)
-					.attr("fill",scenarioColors[i])
-					.attr("cx", function(d) { return x(d.x); })
-					.attr("cy", function(d) { return y(d.y); });
-			}
-
-		}
 	}
 
 }
