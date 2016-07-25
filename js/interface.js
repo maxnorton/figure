fa76f52e91c948c1ca746e323c352c496ea19059
Max Norton <maxbnorton@gmail.com>
2016-07-24 18:07:02 -0700

    hotfix inline glossary links

--- js/interface.js
+++ js/interface.js
@@ -31,19 +31,6 @@ function goBack(e) {
 
     var newHash = window.location.hash;
 
-    /* If the previous page hasn't been loaded in a given time (in this case
-    * 1000ms) the user is redirected to the default location given above.
-    * This enables you to redirect the user to another page.
-    *
-    * However, you should check whether there was a referrer to the current
-    * site. This is a good indicator for a previous entry in the history
-    * session.
-    *
-    * Also you should check whether the old location differs only in the hash,
-    * e.g. /index.html#top --> /index.html# shouldn't redirect to the default
-    * location.
-    */
-
     if(
         newHash === oldHash &&
         (typeof(document.referrer) !== "string" || document.referrer  === "")
@@ -54,10 +41,12 @@ function goBack(e) {
         },1000); // set timeout in ms
     }
     if(e){
-        if(e.preventDefault)
+        if(e.preventDefault) {
             e.preventDefault();
-        if(e.preventPropagation)
+        }
+        if(e.preventPropagation) {
             e.preventPropagation();
+        }
     }
     return false; // stop event propagation and browser default event
 }
@@ -88,7 +77,7 @@ function styleGlossaryLinks() {
 	$('.glossary-link').prepend('<i class="fa fa-question-circle"></i>');
 	$('.glossary-link-inline-child').click( function(event) {
 		event.preventDefault();
-		$(this).stop().children('.glossary-inline').toggle('fast');
+		$(this).stop().find('.glossary-inline').toggle('fast');
 	});
 }
 
