5a84fdc95111cf4dcbadf2f5e28ba82f7ba0f335
Max Norton <maxbnorton@gmail.com>
2016-07-24 18:00:30 -0700

    set up relative backlinks on reference pages

--- js/interface.js
+++ js/interface.js
@@ -23,6 +23,45 @@ function getGenstates() {
 	return genstates;
 }
 
+function goBack(e) {
+    var defaultLocation = "http://maxnorton.github.io/figure";
+    var oldHash = window.location.hash;
+
+    history.back(); // Try to go back
+
+    var newHash = window.location.hash;
+
+    /* If the previous page hasn't been loaded in a given time (in this case
+    * 1000ms) the user is redirected to the default location given above.
+    * This enables you to redirect the user to another page.
+    *
+    * However, you should check whether there was a referrer to the current
+    * site. This is a good indicator for a previous entry in the history
+    * session.
+    *
+    * Also you should check whether the old location differs only in the hash,
+    * e.g. /index.html#top --> /index.html# shouldn't redirect to the default
+    * location.
+    */
+
+    if(
+        newHash === oldHash &&
+        (typeof(document.referrer) !== "string" || document.referrer  === "")
+    ){
+        window.setTimeout(function(){
+            // redirect to default location
+            window.location.href = defaultLocation;
+        },1000); // set timeout in ms
+    }
+    if(e){
+        if(e.preventDefault)
+            e.preventDefault();
+        if(e.preventPropagation)
+            e.preventPropagation();
+    }
+    return false; // stop event propagation and browser default event
+}
+
 function mobileSubstitutions() {
 	if ($(window).width() < 1023) {
 		$('.efficacy-info .table-style').html('<a href="img/efficacy-table01.png" class="swipebox"><i class="fa fa-table" aria-hidden="true"></i> Open reference table');
@@ -46,7 +85,7 @@ function scrollToHash() {
 }
 
 function styleGlossaryLinks() {
-	$('.glossary-link').add('.glossary-link-inline-child').prepend('<i class="fa fa-question-circle"></i>');
+	$('.glossary-link').prepend('<i class="fa fa-question-circle"></i>');
 	$('.glossary-link-inline-child').click( function(event) {
 		event.preventDefault();
 		$(this).stop().children('.glossary-inline').toggle('fast');
